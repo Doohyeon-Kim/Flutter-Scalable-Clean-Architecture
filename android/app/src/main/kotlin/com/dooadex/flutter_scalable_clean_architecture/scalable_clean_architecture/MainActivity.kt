f0b66309490b237629c1a362a861b16b679a2568
doohyeon.kim <doohyeon.kim@doohyeonkims-MacBook-Pro.local>
2022-04-01 13:24:08 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/dooadex/flutter_scalable_clean_architecture/scalable_clean_architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dooadex.flutter_scalable_clean_architecture.scalable_clean_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
